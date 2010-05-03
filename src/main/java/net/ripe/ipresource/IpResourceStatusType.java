021bf8424cff3af6d488a4bfb7e8993f10d1e49d
Paolo Milani <pmilani@ripe.net>
2010-05-03 13:17:53 +0000

    NCC-1883 moved IpResourceStatusType to commons-internet-resources

---
+++ src/main/java/net/ripe/ipresource/IpResourceStatusType.java
@@ -0,0 +1,17 @@
+package net.ripe.ipresource;
+
+public enum IpResourceStatusType {
+
+	// IPv4
+	    ALLOCATED_PA,
+	    ALLOCATED_PI,
+	    ALLOCATED_UNSPECIFIED,
+	    RETURNED_PA,
+	    RETURNED_PI,
+	    RETURNED_UNSPECIFIED,
+	    RESERVED_PA,
+	    PI,
+	// IPv6
+	    ASSIGNED_PI,
+	    ASSIGNED
+}
