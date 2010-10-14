03d2d7a1f4548e6cf082feeabb90fbe4b71af866
Oleg Muravskiy <oleg@ripe.net>
2010-10-14 08:42:42 +0000

    Reverting back inclusion of leading zeroes in ipv6 octets. See NCC#2008032581 and RFC5952, section 4.1:
    4.1. Handling Leading Zeros in a 16-Bit Field
    
    
       Leading zeros MUST be suppressed.  For example, 2001:0db8::0001 is
       not acceptable and must be represented as 2001:db8::1.  A single 16-
       bit 0000 field MUST be represented as 0.

--- src/main/java/net/ripe/ipresource/Ipv6Address.java
+++ src/main/java/net/ripe/ipresource/Ipv6Address.java
@@ -4,7 +4,6 @@ import java.math.BigInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.Validate;
 
 /**
@@ -15,22 +14,22 @@ public class Ipv6Address extends IpAddress {
     private static final long serialVersionUID = 2L;
 
     /**
-	 * Mask for 16 bits, which is the length of one part of an IPv6 address.
-	 */
-	private BigInteger PART_MASK = BigInteger.valueOf(0xffff);
+     * Mask for 16 bits, which is the length of one part of an IPv6 address.
+     */
+    private BigInteger PART_MASK = BigInteger.valueOf(0xffff);
 
     private final BigInteger value;
 
     public Ipv6Address(BigInteger value) {
-		super(IpResourceType.IPv6);
-		this.value = value;
-	}
+        super(IpResourceType.IPv6);
+        this.value = value;
+    }
 
     @Override
     protected int doHashCode() {
         return value.hashCode();
     }
-    
+
     @Override
     protected int doCompareTo(IpResource obj) {
         if (obj instanceof Ipv6Address) {
@@ -58,11 +57,11 @@ public class Ipv6Address extends IpAddress {
     public IpAddress upperBoundForPrefix(int prefixLength) {
         return new Ipv6Address(this.getValue().or(bitMask(prefixLength, getType())));
     }
-    
-	public static Ipv6Address parse(String ipAddressString) {
-	    if (ipAddressString != null) {
-	        ipAddressString = ipAddressString.trim();
-	    }
+
+    public static Ipv6Address parse(String ipAddressString) {
+        if (ipAddressString != null) {
+            ipAddressString = ipAddressString.trim();
+        }
 
         Validate.isTrue(Pattern.matches("[0-9a-fA-F]{0,4}:([0-9a-fA-F]{0,4}:){1,6}[0-9a-fA-F]{0,4}", ipAddressString), "Invalid IPv6 address: " + ipAddressString);
 
@@ -73,10 +72,10 @@ public class Ipv6Address extends IpAddress {
         // The number of double colons must be exactly one if there's a missing colon.
         // The double colon will be the place that gets filled out to complete the address for easy parsing.
         if (colonCount < 7) {
-        	Validate.isTrue(doubleColonCount == 1, "May only be one double colon in an IPv6 address");
+            Validate.isTrue(doubleColonCount == 1, "May only be one double colon in an IPv6 address");
 
-        	// Add extra colons
-        	ipAddressString = expandColons(ipAddressString);
+            // Add extra colons
+            ipAddressString = expandColons(ipAddressString);
         }
 
         // By now we have an IPv6 address that's guaranteed to have 7 colons.
@@ -84,106 +83,108 @@ public class Ipv6Address extends IpAddress {
         return new Ipv6Address(ipv6StringtoBigInteger(ipAddressString));
     }
 
-	/**
-	 * Converts a fully expanded IPv6 string to a BigInteger
-	 *
-	 * @param Fully expanded address (i.e. no '::' shortcut)
-	 * @return Address as BigInteger
-	 */
-	private static BigInteger ipv6StringtoBigInteger(String ipAddressString) {
-		Pattern p = Pattern.compile("([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4})");
+    /**
+     * Converts a fully expanded IPv6 string to a BigInteger
+     *
+     * @param Fully expanded address (i.e. no '::' shortcut)
+     * @return Address as BigInteger
+     */
+    private static BigInteger ipv6StringtoBigInteger(String ipAddressString) {
+        Pattern p = Pattern.compile("([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4}):([0-9a-fA-F]{0,4})");
         Matcher m = p.matcher(ipAddressString);
         m.find();
 
         String ipv6Number = "";
         for (int i = 1; i <= m.groupCount(); i++) {
-        	String part = m.group(i);
-        	String padding = "0000".substring(0, 4 - part.length());
-			ipv6Number = ipv6Number + padding + part;
-		}
+            String part = m.group(i);
+            String padding = "0000".substring(0, 4 - part.length());
+            ipv6Number = ipv6Number + padding + part;
+        }
 
-		return new BigInteger(ipv6Number, 16);
-	}
+        return new BigInteger(ipv6Number, 16);
+    }
 
+    @Override
     public String toString(boolean defaultMissingOctets) {
-    	StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
-    	boolean insideFirstZeroes = false;
-    	boolean afterFirstZeroes = false;
-    	for (int i = 7; i >= 0; i--) {
-    		BigInteger part = getValue().shiftRight(i*16).and(PART_MASK);
-    		if (BigInteger.ZERO.equals(part)) {
-    			if (!insideFirstZeroes && !afterFirstZeroes) {
-    				insideFirstZeroes = true;
-    			} else if (!insideFirstZeroes) {
-    				result.append("0000");
-    			}
-    		} else if (insideFirstZeroes) {
-    			// No longer inside first zero sequence
-    			insideFirstZeroes = false;
-    			afterFirstZeroes = true;
-        		result.append(StringUtils.leftPad(part.toString(16), 4, '0'));
-    		} else {
-        		result.append(StringUtils.leftPad(part.toString(16), 4, '0'));
-    		}
-    		if (i > 0) {
-    			result.append(':');
-    		}
-		}
+        boolean insideFirstZeroes = false;
+        boolean afterFirstZeroes = false;
+        for (int i = 7; i >= 0; i--) {
+            BigInteger part = getValue().shiftRight(i*16).and(PART_MASK);
+            if (BigInteger.ZERO.equals(part)) {
+                if (!insideFirstZeroes && !afterFirstZeroes) {
+                    insideFirstZeroes = true;
+                } else if (!insideFirstZeroes) {
+                    result.append("0");
+                }
+            } else if (insideFirstZeroes) {
+                // No longer inside first zero sequence
+                insideFirstZeroes = false;
+                afterFirstZeroes = true;
+                result.append(part.toString(16));
+            } else {
+                result.append(part.toString(16));
+            }
+            if (i > 0) {
+                result.append(':');
+            }
+        }
 
-    	return compressColons(result.toString());
+        return compressColons(result.toString());
     }
 
 
     // -------------------------------------------------------------------------------- HELPERS
 
-	private String compressColons(String ipv6Address) {
-		// Compress colons into short notation
-		return ipv6Address.replaceFirst(":{3,7}", "::");
-	}
+    private String compressColons(String ipv6Address) {
+        // Compress colons into short notation
+        return ipv6Address.replaceFirst(":{3,7}", "::");
+    }
 
-	public final BigInteger getValue() {
+    @Override
+    public final BigInteger getValue() {
         return value;
     }
-	
-	@Override
-	public boolean isValidNetmask() {
-	    int bitLength = value.bitLength();
-        if (bitLength < IpResourceType.IPv6.getBitSize()) {
-	        return false;
-	    }
-	    
-	    int lowestSetBit = value.getLowestSetBit();
-        for (int i = bitLength - 1; i >= lowestSetBit; --i) {
-	        if (!value.testBit(i)) {
-	            return false;
-	        }
-	    }
 
-	    return true;
-	}
+    @Override
+    public boolean isValidNetmask() {
+        int bitLength = value.bitLength();
+        if (bitLength < IpResourceType.IPv6.getBitSize()) {
+            return false;
+        }
+
+        int lowestSetBit = value.getLowestSetBit();
+        for (int i = bitLength - 1; i >= lowestSetBit; --i) {
+            if (!value.testBit(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     private static String expandColons(String ipv6String) {
-		String filledDoubleColons = ":::::::".substring(0, 7 - countColons(ipv6String) + 2);
-		ipv6String = ipv6String.replace("::", filledDoubleColons);
-		return ipv6String;
-	}
+        String filledDoubleColons = ":::::::".substring(0, 7 - countColons(ipv6String) + 2);
+        ipv6String = ipv6String.replace("::", filledDoubleColons);
+        return ipv6String;
+    }
 
-	private static int countColons(String ipv6String) {
-		Pattern colonPattern = Pattern.compile(":");
+    private static int countColons(String ipv6String) {
+        Pattern colonPattern = Pattern.compile(":");
         Matcher colonMatcher = colonPattern.matcher(ipv6String);
         int colonCount = 0;
         while (colonMatcher.find()) { colonCount++ ; };
-		return colonCount;
-	}
+        return colonCount;
+    }
 
-	private static int numberOfDoubleColons(String ipv6String) {
-		// Count number of double colons: should be either 0 (with 7 colons) or 1 (with less)
+    private static int numberOfDoubleColons(String ipv6String) {
+        // Count number of double colons: should be either 0 (with 7 colons) or 1 (with less)
         Pattern doubleColonPattern = Pattern.compile("::");
         Matcher doubleColonMatcher = doubleColonPattern.matcher(ipv6String);
         int doubleColonCount = 0;
         while (doubleColonMatcher.find()) { doubleColonCount++ ; };
-		return doubleColonCount;
-	}
+        return doubleColonCount;
+    }
 
 }
