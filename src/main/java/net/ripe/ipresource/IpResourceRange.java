0b4318b08c57b9d69ccebb1590e9a20fba654edc
Erik Rozendaal <cac2er@ripe.net>
2012-08-28 15:29:29 +0000

    Layout fixes.

--- src/main/java/net/ripe/ipresource/IpResourceRange.java
+++ src/main/java/net/ripe/ipresource/IpResourceRange.java
@@ -69,14 +69,17 @@ public class IpResourceRange extends IpResource {
     // ------------------------------------------------------------ Range
     // Interface
 
+    @Override
     public UniqueIpResource getStart() {
         return start;
     }
 
+    @Override
     public UniqueIpResource getEnd() {
         return end;
     }
 
+    @Override
     public IpResourceType getType() {
         return start.getType();
     }
@@ -85,18 +88,17 @@ public class IpResourceRange extends IpResource {
     public boolean isValidNetmask() {
         return isUnique() && unique().isValidNetmask();
     }
-    
+
     @Override
     public String toString() {
         return start.toString() + "-" + end.toString();
     }
 
     @Override
-    protected int doCompareTo(IpResource obj) {
-        if (obj instanceof UniqueIpResource) {
-            return -obj.compareTo(this);
-        } else if (obj instanceof IpResourceRange) {
-            IpResource that = (IpResource) obj;
+    protected int doCompareTo(IpResource that) {
+        if (that instanceof UniqueIpResource) {
+            return -that.compareTo(this);
+        } else if (that instanceof IpResourceRange) {
             int rc = getStart().doCompareTo(that.getStart());
             if (rc != 0) {
                 return rc;
@@ -104,7 +106,7 @@ public class IpResourceRange extends IpResource {
                 return -getEnd().doCompareTo(that.getEnd());
             }
         } else {
-            throw new IllegalArgumentException("unknown resource type: " + obj);
+            throw new IllegalArgumentException("unknown resource type: " + that);
         }
     }
 
@@ -129,14 +131,14 @@ public class IpResourceRange extends IpResource {
      *   <li>IpResourceRange.parse("10.0.0.0/16,/24,/25,/31") => Parses 10.0.0.0/16 plus the adjacent networks /24, /25 and /31. Note that the mask of these networks MUST increase. I.e. go from big to small..
      *   <li>IpResourceRange.parse("AS0-AS4294967295") => For AS numbers ranges may be used
      * </ul>
-     * 
+     *
      * @see IpResourceSet
      */
     public static IpResourceRange parse(String s) {
         if(s.indexOf(',') >= 0) {
         	return parseCommaPrefixNotation(s);
         }
-        
+
         if (s.indexOf('/') >= 0) {
         	return parseAsSingleSlashNotatedRange(s);
         }
@@ -178,16 +180,16 @@ public class IpResourceRange extends IpResource {
 	 */
 	private static IpResourceRange parseCommaPrefixNotation(String s) {
 		int slashIdx = s.indexOf('/');
-		
+
 		if (slashIdx == -1) {
 			throw new IllegalArgumentException("Comma separated notation can only be used for adjacent prefix notations like: 10.0.0.0/16,/24,/25,/31");
 		}
-		
+
 		String startAddress = s.substring(0, slashIdx);
-		
+
 		UniqueIpResource start = UniqueIpResource.parse(startAddress);
 		UniqueIpResource end = UniqueIpResource.parse(startAddress);
-		
+
 		String prefixSizeList = s.substring(slashIdx);
 		int lastSeenPrefixMask = -1;
 		for(String prefixStr: prefixSizeList.split(",")) {
@@ -199,10 +201,10 @@ public class IpResourceRange extends IpResource {
 				throw new IllegalArgumentException("Mask of prefix " + prefixMask + " is bigger than previous: " + lastSeenPrefixMask);
 			}
 		}
-		
+
 		return IpResourceRange.range(start, end.predecessor());
 	}
-    
+
     public static IpResourceRange assemble(BigInteger start, BigInteger end, IpResourceType type) {
         return type.fromBigInteger(start).upTo(type.fromBigInteger(end));
     }
@@ -213,7 +215,7 @@ public class IpResourceRange extends IpResource {
 		if (!netmask.isValidNetmask()) {
 		    throw new IllegalArgumentException("netmask '" + netmaskStr + "' is not a valid netmask");
 		}
-		
+
 		int size = netmask.getValue().bitCount();
         UniqueIpResource end = start.upperBoundForPrefix(size);
 
